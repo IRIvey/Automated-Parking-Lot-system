4850df928ee600e8c5b8a0a0013cc51ec7f6f5ba
IRIvey <isrativey@iut-dhaka.edu>
2025-03-24 16:31:28 +0600

    File Refactored

--- ParkingCLI.java
+++ ParkingCLI.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 
-
 public class ParkingCLI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Command processor = new Command();
+
+        System.out.print("Enter parking lot size: ");
+        int size = scanner.nextInt();
+        scanner.nextLine();
+
+        ParkingLotManager parkingLot = new ParkingLotManager(size);
+        ParkingQueryManager queryService = new ParkingQueryManager(parkingLot.getOccupiedSlots());
+
+        CommandProcessor processor = new CommandProcessor();
+        processor.registerCommand("park", new ParkCommand(parkingLot));
+        processor.registerCommand("leave", new LeaveCommand(parkingLot));
 
         while (true) {
             String command = scanner.nextLine();

169a3cb8931d9c338968669999312edd81de1ef7
IRIvey <isrativey@iut-dhaka.edu>
2025-03-24 17:16:05 +0600

    File Refactored

---
+++ CreateParkingLotCommand.java
@@ -0,0 +1,18 @@
+class CreateParkingLotCommand implements Command {
+    private CommandProcessor processor;
+
+    public CreateParkingLotCommand(CommandProcessor processor) {
+        this.processor = processor;
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length == 1) {
+            int capacity = Integer.parseInt(args[0]);
+            processor.setParkingLot(ParkingLotFactory.createParkingLot(capacity));
+            System.out.println("Created a parking lot with " + capacity + " slots");
+        } else {
+            System.out.println("Invalid command syntax");
+        }
+    }
+}

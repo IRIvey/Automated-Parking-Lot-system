326e3ce635ee03c5658af83fb3008cee4074e70d
IRIvey <isrativey@iut-dhaka.edu>
2025-03-24 15:52:15 +0600

    File Refactored

---
+++ ParkCommand.java
@@ -0,0 +1,14 @@
+class ParkCommand implements Command {
+    private IParkingLot parkingLot;
+
+    public ParkCommand(IParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length == 2) {
+            parkingLot.parkCar(args[0], args[1]);
+        }
+    }
+}

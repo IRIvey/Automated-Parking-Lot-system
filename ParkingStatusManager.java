bc13bee6d1170482000b0b602c04519fd4395f9c
IRIvey <isrativey@iut-dhaka.edu>
2025-03-25 20:31:19 +0600

    File Modified

---
+++ ParkingStatusManager.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class ParkingStatusManager {
+    private TreeMap<Integer, Ticket> occupiedSlots;
+
+    public ParkingStatusManager(TreeMap<Integer, Ticket> occupiedSlots) {
+        this.occupiedSlots = occupiedSlots;
+    }
+
+    public List<String> getStatus() {
+        List<String> statusList = new ArrayList<>();
+        if (occupiedSlots.isEmpty()) {
+            statusList.add("Parking lot is empty");
+        } else {
+            statusList.add("Slot No. | Registration No. | Colour");
+            for (Map.Entry<Integer, Ticket> entry : occupiedSlots.entrySet()) {
+                statusList.add(entry.getKey() + " | " +
+                        entry.getValue().getCar().getRegistrationNo() + " | " +
+                        entry.getValue().getCar().getColor());
+            }
+        }
+        return statusList;
+    }
+}
+
